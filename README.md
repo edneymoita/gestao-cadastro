a8c93600bc32817ea8b9ed7d7d159c27d2b0f933
edneymoita <edneymoita@gmail.com>
2021-07-23 18:51:04 -0300

    Telas reformuladas, funcionalidades revisadas, README ajustado

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# gestao-cadastro
- Cadastro de Cliente e Produto
+# Gestão de Cadastro - Cliente e Produto
+
+Módulo simples para cadastro de cliente e produto, armazenados no formato JSON no localStorage.
+
+## Instalação
+
+Aplicação Web. Não necessita de instalação.
+
+## URL
+
+[https://gestao-cadastro-edneymoita.vercel.app/](https://gestao-cadastro-edneymoita.vercel.app/)
+
+## Funcionalidades
+
+Na tela inicial escolha cliente ou produto para cadastrar um novo item. Preencha os dados solicitados e clique no botão **Grava Cliente/Produto**.
+
+Caso queira verificar se já existe algum cliente/produto cadastrado, clique no botão **Recupera Cliente/Produto**.
+
+Caso queira apagar o cliente/produto que está cadastrado, clique no botão **Apaga Cliente/Produto**.
+
+## Licença
+[EM](https://github.com/edneymoita)
--- index.js
+++ index.js
@@ -1,111 +1,150 @@
-function getProduct(){
-    const product = JSON.parse(localStorage.getItem('product'));
-
-    if (product == null){
-        document.getElementById('msg').innerHTML = 'Não existe produto armazenado';
-
-        document.getElementById('labelBox').value = '';
-        document.getElementById('brandBox').value = '';
-        document.getElementById('presentationBox').value = '';
-        document.getElementById('volumeBox').value = '';
-        document.getElementById('amountBox').value = '';
-        document.getElementById('netPriceBox').value = '';
-        document.getElementById('retailPriceBox').value = '';
-    }
-    else {
-        document.getElementById('labelBox').value = product.label;
-        document.getElementById('brandBox').value = product.brand;
-        document.getElementById('presentationBox').value = product.presentation;
-        document.getElementById('volumeBox').value = product.volume;
-        document.getElementById('amountBox').value = product.amount;
-        document.getElementById('netPriceBox').value = product.netPrice;
-        document.getElementById('retailPriceBox').value = product.retailPrice;
-    }
-}
-
-function saveProduct(){
-    const label = document.getElementById('labelBox').value;
-    const brand = document.getElementById('brandBox').value;
-    const presentation = document.getElementById('presentationBox').value;
-    const volume = document.getElementById('volumeBox').value;
-    const amount = document.getElementById('amountBox').value;
-    const netPrice = document.getElementById('netPriceBox').value;
-    const retailPrice = document.getElementById('retailPriceBox').value;
-
-    let product ={
-        "label": label,
-        "brand": brand,
-        "presentation": presentation,
-        "volume": volume,
-        "amount": amount,
-        "netPrice": netPrice,
-        "retailPrice": retailPrice,
-    };
-
-    localStorage.setItem('product', JSON.stringify(product));
-
-    document.getElementById('msg').innerHTML = 'Produto gravado com sucesso!';
-}
-
-function deleteProduct(){
-    if (localStorage.getItem('product') != null) {
-        localStorage.removeItem('product');
-        document.getElementById('msg').innerHTML = 'Produto apagado com sucesso!';
-    }
-    else
-        document.getElementById('msg').innerHTML = 'Não existia produto armazenado!';
-}
-
 function getClient(){
     const client = JSON.parse(localStorage.getItem('client'));
 
     if (client == null){
         document.getElementById('msg').innerHTML = 'Não existe cliente armazenado';
 
-        document.getElementById('nameBox').value = '';
-        document.getElementById('addressBox').value = '';
-        document.getElementById('emailBox').value = '';
-        document.getElementById('phoneBox').value = '';
-        document.getElementById('cpfBox').value = '';
-        document.getElementById('rgBox').value = '';
+        clearClientForm();
     }
     else {
         document.getElementById('nameBox').value = client.name;
         document.getElementById('addressBox').value = client.address;
-        document.getElementById('emailBox').value = client.email;
+        document.getElementById('numberBox').value = client.number;
+        document.getElementById('districtBox').value = client.district;
+        document.getElementById('cityBox').value = client.city;
+        document.getElementById('stateBox').value = client.state;
+        document.getElementById('cepBox').value = client.cep;
         document.getElementById('phoneBox').value = client.phone;
-        document.getElementById('cpfBox').value = client.cpf;
-        document.getElementById('rgBox').value = client.rg;
+        document.getElementById('emailBox').value = client.email;
     }
+    document.getElementById('nameBox').focus();
 }
 
 function saveClient(){
     const name = document.getElementById('nameBox').value;
     const address = document.getElementById('addressBox').value;
-    const email = document.getElementById('emailBox').value;
+    const number = document.getElementById('numberBox').value;
+    const district = document.getElementById('districtBox').value;
+    const city = document.getElementById('cityBox').value;
+    const state = document.getElementById('stateBox').value;
+    const cep = document.getElementById('cepBox').value;
     const phone = document.getElementById('phoneBox').value;
-    const cpf = document.getElementById('cpfBox').value;
-    const rg = document.getElementById('rgBox').value;
+    const email = document.getElementById('emailBox').value;
 
-    let client = {
-        'name': name,
-        'address': address,
-        'email': email,
-        'phone': phone,
-        'cpf': cpf,
-        'rg': rg,
+    if (name != '') {
+        let client = {
+            'name': name,
+            'address': address,
+            'number': number,
+            'district': district,
+            'city': city,
+            'state': state,
+            'cep': cep,
+            'phone': phone,
+            'email': email,
+        }
+
+        localStorage.setItem('client', JSON.stringify(client));
+
+        document.getElementById('msg').innerHTML = 'Cliente gravado com sucesso!';
     }
-
-    localStorage.setItem('client', JSON.stringify(client));
-
-    document.getElementById('msg').innerHTML = 'Cliente gravado com sucesso!';
+    else {
+        alert('Favor informar o nome do cliente!');
+        document.getElementById('nameBox').focus();
+    }
 }
 
 function deleteClient(){
+    clearClientForm();
+
     if (localStorage.getItem('client') != null) {
         localStorage.removeItem('client');
         document.getElementById('msg').innerHTML = 'Cliente apagado com sucesso!';
     }
     else
         document.getElementById('msg').innerHTML = 'Não existia cliente armazenado!';
+    document.getElementById('nameBox').focus();
+}
+
+function clearClientForm() {
+    document.getElementById('nameBox').value = '';
+    document.getElementById('addressBox').value = '';
+    document.getElementById('numberBox').value = '';
+    document.getElementById('districtBox').value = '';
+    document.getElementById('cityBox').value = '';
+    document.getElementById('stateBox').value = '';
+    document.getElementById('cepBox').value = '';
+    document.getElementById('phoneBox').value = '';
+    document.getElementById('emailBox').value = '';
+
+    document.getElementById('nameBox').focus();
+}
+
+function getProduct(){
+    const product = JSON.parse(localStorage.getItem('product'));
+
+    if (product == null){
+        document.getElementById('msg').innerHTML = 'Não existe produto armazenado';
+
+        clearProductForm();
+    }
+    else {
+        document.getElementById('codeBox').value = product.code;
+        document.getElementById('nameBox').value = product.name;
+        document.getElementById('amountBox').value = product.amount;
+        document.getElementById('priceBox').value = product.price;
+        document.getElementById('descriptionBox').value = product.description;
+    }
+}
+
+function saveProduct(){
+    const code = document.getElementById('codeBox').value;
+    const name = document.getElementById('nameBox').value;
+    const amount = document.getElementById('amountBox').value;
+    const price = document.getElementById('priceBox').value;
+    const description = document.getElementById('descriptionBox').value;
+
+    if (code != '' && name != ''){
+        let product ={
+            "code": code,
+            "name": name,
+            "amount": amount,
+            "description": description,
+            "price": price,
+        };
+
+        localStorage.setItem('product', JSON.stringify(product));
+
+        document.getElementById('msg').innerHTML = 'Produto gravado com sucesso!';
+    }
+    else {
+        alert('Favor informe código e nome do produto!');
+        document.getElementById('codeBox').focus();
+    }
+}
+
+function deleteProduct(){
+    clearProductForm();
+    if (localStorage.getItem('product') != null) {
+        localStorage.removeItem('product');
+        document.getElementById('msg').innerHTML = 'Produto apagado com sucesso!';
+    }
+    else
+        document.getElementById('msg').innerHTML = 'Não existia produto armazenado!';
+
+    document.getElementById('codeBox').focus();
+}
+
+function clearProductForm(){
+    document.getElementById('codeBox').value = '';
+    document.getElementById('nameBox').value = '';
+    document.getElementById('amountBox').value = '';
+    document.getElementById('priceBox').value = '';
+    document.getElementById('descriptionBox').value = '';
+
+    document.getElementById('codeBox').focus();
+}
+
+function goBack () {
+    window.history.back();
 }
